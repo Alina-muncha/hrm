f16d8fec98a7a9354fa04671fe571f213c0713de
alina-muncha <alina.muncha@gmail.com>
2022-07-10 17:19:51 +0545

    init

--- resources/views/layouts/clock-in-portal/holidays.blade.php
+++ resources/views/layouts/clock-in-portal/holidays.blade.php
@@ -39,6 +39,7 @@
 									</ul>
 									<!--end::Breadcrumb-->
 								</div>
+								
 								<!--end::Page title-->
 								<!--begin::Actions-->
 								<div class="d-flex align-items-center gap-2 gap-lg-3">
